3057bdeee1e07108118ccfce5c5d464dd6cd6468
IetsCools420 <daan.renken@gmail.com>
2018-12-11 13:20:25 +0100

    fixed

--- MultiClientServer/MultiClientServer/Program.cs
+++ MultiClientServer/MultiClientServer/Program.cs
@@ -14,12 +14,38 @@ namespace MultiClientServer
         static void Main(string[] args)
         {
             Console.Write("Op welke poort ben ik server? ");
+            //MijnPoort = int.Parse(args[0]);
             MijnPoort = int.Parse(Console.ReadLine());
             new Server(MijnPoort);
             Console.Title = MijnPoort.ToString();
+
+            /*
+            for (int i = 1; i < args.Length; i++)
+            {
+                int newPoort = int.Parse(args[i]);
+                bool connectSucceed = false;
+                while (!connectSucceed)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    try
+                    {
+                        if (Buren.ContainsKey(newPoort))
+                            Console.WriteLine("Hier is al verbinding naar!");
+                        else
+                        {
+                            // Leg verbinding aan (als client)
+                            Buren.Add(newPoort, new Connection(newPoort));
+                        }
+                        connectSucceed = true;
+                    }
+                    catch { Exception e; }
+                }
+            }
+            */
+
             Console.WriteLine("Typ [verbind poortnummer] om verbinding te maken, bijvoorbeeld: verbind 1100");
             Console.WriteLine("Typ [poortnummer bericht] om een bericht te sturen, bijvoorbeeld: 1100 hoi hoi");
-           
+
             while (true)
             {
                 string input = Console.ReadLine();
@@ -37,7 +63,7 @@ namespace MultiClientServer
                             Buren.Add(poort, connection);
                             Connecties.Add(poort);
                             connection.ping = connection.Ping(poort);
-                            
+
                         }
                     }
                     else if (input.StartsWith("ping"))
@@ -77,13 +103,13 @@ namespace MultiClientServer
                 //    Console.WriteLine("foute input probeer het nog eens");
                 //}
             }
-            
+
         }
         static void print()
         {
             for (int i = 0; i < Connecties.Count; i++)
             {
-               Console.WriteLine(Connecties[i] + " " + Buren[Connecties[i]].ping);
+                Console.WriteLine(Connecties[i] + " " + Buren[Connecties[i]].ping);
             }
         }
     }
